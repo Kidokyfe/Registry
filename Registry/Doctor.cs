730968bda882d51c4e8e5480ab2b88c4bf5d28c9
Artem <cinquestyle@gmail.com>
2015-05-14 22:48:32 +0300

    Dialog

---
+++ Registry/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registry
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public int Cab { get; set; }
+        public string Mo { get; set; }
+        public string Tu { get; set; }
+        public string We { get; set; }
+        public string Th { get; set; }
+        public string Fr { get; set; }
+        public string Sa { get; set; }
+        public string Su { get; set; } 
+    }
+}
--- Registry/MainWindow.xaml.cs
+++ Registry/MainWindow.xaml.cs
@@ -21,35 +21,34 @@ namespace Registry
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class Doctor
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Speciality { get; set; }
-            public int Cab { get; set; }
-            public string Mo { get; set; }
-            public string Tu { get; set; }
-            public string We { get; set; }
-            public string Th { get; set; }
-            public string Fr { get; set; }
-            public string Sa { get; set; }
-            public string Su { get; set; }            
-            
-        }
+        public List<Doctor> Doctors = new List<Doctor>();
         public MainWindow()
         {
             InitializeComponent();
-            var doctors = new List<Doctor>
-            {
-                new Doctor {Id = 1, Name = "Ivanov AC", Speciality = "Dentis", Cab = 101, Mo = "12:00-16:00"},
-                new Doctor {Id = 2, Name = "Sidorov KV", Speciality = "Therapist", Cab = 102, Mo = "08:00-12:00"},
-                new Doctor {Id = 3, Name = "Petrova AI", Speciality = "Surgeon", Cab = 103, We = "12:00-16:00"},
-                new Doctor {Id = 4, Name = "Smirnova VA", Speciality = "Urologist", Cab = 104, Tu = "08:00-12:00"},
-                new Doctor {Id = 5, Name = "Locev NZ", Speciality = "Cardiologist", Cab = 105, Th = "12:00-16:00", Fr = "08:00-12:00"},
-                new Doctor {Id = 6, Name = "Abragimova DS", Speciality = "Otolaryngologist", Cab = 106, Tu = "12:00-16:00"}
-            };
+            InitializeTable();
+        }
 
-            Table.ItemsSource = doctors;            
+        private void InitializeTable()
+        {
+            Doctors.Add(new Doctor {Id = 1, Name = "Ivanov AC", Speciality = "Dentis", Cab = 101, Mo = "12:00-16:00"});
+            Doctors.Add(new Doctor {Id = 2, Name = "Sidorov KV", Speciality = "Therapist", Cab = 102, Mo = "08:00-12:00"});
+            Doctors.Add(new Doctor {Id = 3, Name = "Petrova AI", Speciality = "Surgeon", Cab = 103, We = "12:00-16:00"});
+            Doctors.Add(new Doctor {Id = 4, Name = "Smirnova VA", Speciality = "Urologist", Cab = 104, Tu = "08:00-12:00"});
+            Doctors.Add(new Doctor {Id = 5, Name = "Locev NZ", Speciality = "Cardiologist", Cab = 105, Th = "12:00-16:00", Fr = "08:00-12:00"});
+            Doctors.Add(new Doctor {Id = 6, Name = "Abragimova DS", Speciality = "Otolaryngologist", Cab = 106, Tu = "12:00-16:00"});
+
+            Table.ItemsSource = Doctors;
+        }
+
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoctorDialog dialog = new DoctorDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                Doctors.Add(new Doctor() { Name = dialog.Name.Text });
+            }            
+            
         }
     }
 }

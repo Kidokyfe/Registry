7e2b5bc7e759d2805aea3f668ac2c5b9b19c1270
Artem <cinquestyle@gmail.com>
2015-05-25 20:03:33 +0300

    DoctorList (fail add file)

---
+++ Registry/DoctorList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using DoctorLibrary;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Registry
+{
+    public class DoctorList : List<Doctor>
+    {
+        public void Info()
+        {
+            StringBuilder info = new StringBuilder();
+
+            var mans = from d in this
+                       where d.Name[d.Name.IndexOf(' ') - 1] != 'a'
+                       select d;
+            var doctors = mans as Doctor[] ?? mans.ToArray();
+            info.Append(String.Format("Mans: ({0})\n", doctors.Count()));
+            foreach (var e in doctors)
+            {
+                info.Append(e.Name + '\n');
+            }
+
+            info.Append('\n');
+
+            
+            var womans = from d in this
+                where d.Name[d.Name.IndexOf(' ') - 1] == 'a'
+                select d;
+            doctors = womans as Doctor[] ?? womans.ToArray();
+            info.Append(String.Format("Womans: ({0})\n", doctors.Count()));
+            foreach (var e in doctors)
+            {
+                info.Append(e.Name + '\n');
+            }
+
+            MessageBox.Show(info.ToString());
+        }
+    }
+}
